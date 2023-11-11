574e12ea19b216b19c9598757c09b00bcb53da87
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2023-11-11 20:33:06 +0530

    updated ui, made email changes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,13 @@
 import Button from "@mui/material/Button";
 import Frame from "../assets/Frame.svg";
 import arrow from "../assets/arrow.svg";
-import EastSharpIcon from "@mui/icons-material/EastSharp";
+
 
 const Landing = () => {
   return (
-    <div className="flex mx-14 mt-[29px] ">
+    <div className="flex mx-14  mt-[29px] w-screen ">
       <div className=" ">
-        <div className="relative w-[881px] h-[879px] bg-black rounded-[16px]">
+        <div className="relative w-[55.1rem]  bg-black rounded-[16px]">
           <div className="inline-flex flex-col items-start gap-[8px] p-[8px] relative top-[35px] left-[79px]">
             <div className="flex flex-col w-[713px] items-center gap-[40px] relative flex-[0_0_auto]">
               <p className="relative w-fit mt-[-1.00px] [font-family:'Inter-Bold',Helvetica] font-bold text-gray-1 text-[36px] text-center tracking-[0] leading-[42px] whitespace-nowrap text-white">
@@ -17,7 +17,7 @@ const Landing = () => {
           </div>
         </div>
       </div>
-      <div className="w-[426px] h-[576px] ml-5 rounded-2xl bg-primary-blue px-4 py-14 ">
+      <div className="w-[20.4375rem] h-[4.25rem] ml-5 rounded-2xl bg-primary-blue px-4 py-14 ">
         <div className="w-64 mx-auto text-center flex flex-col justify-center items-center">
           <p className="fixed[font-family:'Inter-Bold',Helvetica] font-bold text-white w-72 h-20 text-[24px]  ">
             Get Started with KoinX for FREE
